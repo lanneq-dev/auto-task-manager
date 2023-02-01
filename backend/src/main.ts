c573466b65dfcbfea9c53228d6a102c402bfce38
lanneq <0xdf00ff@gmail.com>
2023-02-02 02:16:38 +0500

    Swagger added

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('👾 AUTO TASK MANAGER')
+    .setDescription('Task manager with reminders in messengers.')
+    .setVersion('v0.1')
+    .addTag('task')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
