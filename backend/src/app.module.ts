2ebf7a4d61d63a5ba6ed9d6475cc1e6cac2c7b3c
lanneq <0xdf00ff@gmail.com>
2023-02-02 05:00:05 +0500

    CRUD. Swagger. Rest API

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { ArticlesModule } from './articles/articles.module';
+import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, ArticlesModule, TaskModule],
   controllers: [AppController],
   providers: [AppService],
 })
